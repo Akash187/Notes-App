52f55430db90bbebc4d74ffcdd59ce0aee3e0062
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-01-02 01:11:01 +0530

    Lecture 16 Adding and Saving Notes.

--- notes.js
+++ notes.js
@@ -1,9 +1,27 @@
 console.log('Starting notes.js');
 
 //module object is default object in every node file
+const fs = require('fs');
 
 let addNote = (title, body) => {
-    console.log('Adding Note', title, body);
+    let notes = [];
+    let note = {
+        title,
+        body
+    };
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    }catch(e){
+
+    }
+
+    let duplicateNotes = notes.filter((note) => note.title === title);
+
+    if(duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 let getAll = () => {
